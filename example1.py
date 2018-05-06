fc5b314f713b8a7330ad929eb6f99bed5dfb4d6e
Yeliang Li <yeliang_li@126.com>
2018-05-06 23:32:16 +0800

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author: YeliangLi
+  Created: 2018/5/6
+"""
+
 import tensorflow as tf
 
 class CustomCell(tf.contrib.rnn.RNNCell):
@@ -65,7 +72,7 @@ if __name__ == "__main__":
         print(o3)
 
 '''
-Êä³ö½á¹û£º
+ÃŠÃ¤Â³Ã¶Â½Ã¡Â¹Ã»Â£Âº
 
 [[[ 1.]
   [ 3.]
